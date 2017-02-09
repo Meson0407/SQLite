b92fce310eaa6a01dde6c25f1124e33940d46978
XueTao <451831819@qq.com>
2017-02-09 10:17:38 +0800

    Initial Commit

---
+++ SQLite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLite
+//
+//  Created by 薛涛 on 17/2/9.
+//  Copyright © 2017年 Xuetao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
